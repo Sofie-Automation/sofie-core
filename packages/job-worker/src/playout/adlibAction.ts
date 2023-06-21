e0135c36c40a14455c11993f06e834b2c3ffbe6e
Ivan Maretić <i.maretic@evs.com>
2023-06-21 17:18:45 +0200

    feat: return adlib action validation errors to client

--- packages/job-worker/src/playout/adlibAction.ts
+++ packages/job-worker/src/playout/adlibAction.ts
@@ -243,7 +243,7 @@ export async function executeActionInner(
 	try {
 		const blueprintPersistentState = new PersistentPlayoutStateStore(playoutModel.playlist.previousPersistentState)
 
-		await blueprint.blueprint.executeAction(
+		const result = await blueprint.blueprint.executeAction(
 			actionContext,
 			blueprintPersistentState,
 			actionParameters.actionId,
@@ -253,6 +253,14 @@ export async function executeActionInner(
 			actionParameters.publicData,
 			actionParameters.actionOptions ?? {}
 		)
+		if (result && result.validationErrors) {
+			throw UserError.from(
+				new Error(`AdLib Action validation failed: ${JSON.stringify(result.validationErrors)}`),
+				UserErrorMessage.ValidationFailed,
+				undefined,
+				409
+			)
+		}
 
 		if (blueprintPersistentState.hasChanges) {
 			playoutModel.setBlueprintPersistentState(blueprintPersistentState.getAll())
