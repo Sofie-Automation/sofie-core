93e88f3448f15b0bc00d6bc99961515ab2344eda
András Pozsgai <code@justandras.com>
2025-10-20 13:53:04 +0200

    chore: revert changes that are not needed

--- packages/live-status-gateway/src/liveStatusServer.ts
+++ packages/live-status-gateway/src/liveStatusServer.ts
@@ -130,7 +130,7 @@ export class LiveStatusServer {
 		rootChannel.addTopic(SubscriptionName.RESERVED_PACKAGES, packageStatusTopic)
 		rootChannel.addTopic(SubscriptionName.BUCKETS, bucketsTopic)
 
-		const wss = new WebSocketServer({ port: 9090 })
+		const wss = new WebSocketServer({ port: 8080 })
 		wss.on('connection', (ws, request) => {
 			this._logger.info(`WebSocket connection requested for path '${request.url}'`)
 
