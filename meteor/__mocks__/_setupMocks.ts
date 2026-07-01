f6755613931c35585e73fc79f60dfbbd6fd2d06e
Julian Waller <git@julusian.co.uk>
2026-07-01 18:08:53 +0100

    wip: tidy

--- meteor/__mocks__/_setupMocks.ts
+++ meteor/__mocks__/_setupMocks.ts
@@ -11,14 +11,8 @@ jest.mock('nanoid', (...args) => require('./random').setup(args), { virtual: tru
 // Add references to all "meteor" mocks below, so that jest resolves the imports properly.
 
 jest.mock('meteor/meteor', (...args) => require('./meteor').setup(args), { virtual: true })
-jest.mock('meteor/random', (...args) => require('./random').setup(args), { virtual: true })
 jest.mock('meteor/check', (...args) => require('./check').setup(args), { virtual: true })
 jest.mock('meteor/ejson', (...args) => require('./meteor-ejson').setup(args), { virtual: true })
-
-jest.mock('meteor/mdg:validated-method', (...args) => require('./validated-method').setup(args), { virtual: true })
-jest.mock('meteor/julusian:meteor-elastic-apm', (...args) => require('./meteor-elastic-apm').setup(args), {
-	virtual: true,
-})
 
 jest.mock('meteor/webapp', (...args) => require('./webapp').setup(args), { virtual: true })
 
